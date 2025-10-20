4d3b848a0d7c25d1befb21c0f28189332975ade7
gibumgo <gibumgo@gmail.com>
2025-10-20 19:43:00 +0900

    refactor: Number 클래스를 domain 패키지로 이동

--- src/main/java/calculator/domin/Number.java
+++ src/main/java/calculator/domin/Number.java
@@ -1,4 +1,4 @@
-package calculator;
+package calculator.domin;
 
 public class Number {
     private static final int MIN_NUMBER = 0;
--- src/test/java/calculator/domin/NumberTest.java
+++ src/test/java/calculator/domin/NumberTest.java
@@ -1,4 +1,4 @@
-package calculator;
+package calculator.domin;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
